954539b348b0742c1db9407ed99104629b2917b1
Rafal Adamczyk <rafal.adamczyk44@gmail.com>
2025-04-12 17:07:08 +0200

    Adding history tracking to Conversation Manager class

--- converstaion_manager.py
+++ converstaion_manager.py
@@ -1,5 +1,3 @@
-
-
 class ConversationManager:
     def __init__(self):
         self.user_name = None
@@ -9,10 +7,29 @@ class ConversationManager:
         self.booking_date_time = None
         self.booking_location = None
 
-        # Keep track of last question
+        # For state tracking
+        self.current_state = 'greetings'
         self.last_question = None
+        self.history = []
 
-    def extract_info(self, extracted_info):
+    def update_state(self, new_state):
+        """Switch to new state, update the history"""
+        self.history.append((self.current_state, new_state))
+        self.current_state = new_state
+
+    def add_user_history(self, query):
+        self.history.append({'role': 'user',
+                             'query': query,})
+
+    def add_sys_history(self, query):
+        self.history.append({'role': 'system',
+                             'query': query,})
+
+    def extract_info(self, extracted_info: dict):
+        """
+        Extract information from extracted_info from user query
+        :param extracted_info: Dictionary with extracted information from the query
+        """
         if 'name' in extracted_info and extracted_info['name']:
             self.user_name = extracted_info['name']
 
@@ -31,8 +48,11 @@ class ConversationManager:
         if 'booking_location' in extracted_info and extracted_info['booking_location']:
             self.booking_location = extracted_info['booking_location']
 
-
-    def check_missing_info(self):
+    def check_missing_info(self) -> list:
+        """
+        Check the current missing info, direct the conversation accordingly
+        :return: List of missing info
+        """
         missing_info = []
         if not self.user_name:
             missing_info.append("ask_name")
@@ -50,12 +70,17 @@ class ConversationManager:
             missing_info.append("get_location")
         return missing_info
 
-    def return_details(self):
-        return f"""
-            Time of booking: {self.booking_date_time},
-            Booking in the name of {self.user_name},
-            Preferred cuisine: {self.culinary_preferences},
-            Dietary: {self.dietary_preferences},
-            Party size: {self.party_size},
-            Location: {self.booking_location}.
-        """
+    def return_history(self) -> list:
+        """Return full history of conversation intent based"""
+        return self.history
+
+    def return_details(self) -> dict:
+        """Return booking details"""
+        return {
+            'user_name': self.user_name,
+            'booking_time': self.booking_date_time,
+            'booking_location': self.booking_location,
+            'party_size': self.party_size,
+            'dietary_preferences': self.dietary_preferences,
+            'culinary_preferences': self.culinary_preferences,
+        }
